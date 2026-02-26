13e149cb033574904b47493dd6e3abafc11e08e6
tarstars <142857@mai.ru>
2026-02-26 16:20:15 +0300

    Add sitemap_index.xml endpoint

---
+++ src/pages/sitemap_index.xml.ts
@@ -0,0 +1,15 @@
+export function GET() {
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>https://phys-math.dev/sitemap.xml</loc>
+  </sitemap>
+</sitemapindex>
+`;
+
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8'
+    }
+  });
+}
